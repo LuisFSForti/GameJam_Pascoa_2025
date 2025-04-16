97f1b665a79da9aaebe01831fc0e38405922e847
Lucien <89356326+Ala-R-F@users.noreply.github.com>
2025-04-16 17:23:06 -0300

    Inimigo Milho

---
+++ GameJam_Pascoa_2025/Assets/Scripts/Inimigos/Projeteis/BalaController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class BalaController : MonoBehaviour
+{
+    [Header("PosicaoCoelho")]
+    [SerializeField] private Transform _coelhoPosition;
+
+    [Header("Movimentacao")]
+    [SerializeField] private float _velocidade;
+
+    [SerializeField] private Rigidbody2D _rigidbody2d;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        _coelhoPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        Vector2 direction = (_coelhoPosition.transform.position - transform.position).normalized;
+        _rigidbody2d.linearVelocity = direction * _velocidade;  // Use velocity, not linearVelocity
+    }
+
+    public void setVelocidade(float _velocidadeDada){
+        _velocidade = _velocidadeDada;
+    }
+
+
+    void OnBecomeInvisible(){
+        
+            Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
